97601e69277852fbda411b75bdeef82e600ed30e
unknown <Ullas@Ullas-PC.(none)>
2012-06-01 15:48:11 +0530

    ver 1.0   Please enter the commit message for your changes. Lines starting

---
+++ alendar/src/com/mht/calendartactivity/AlendarActivity.java
@@ -0,0 +1,13 @@
+package com.mht.calendartactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlendarActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
